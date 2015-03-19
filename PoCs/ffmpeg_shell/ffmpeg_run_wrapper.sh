6eee5cc0bc47d3bd36f603f8697772238b763922
andrew-szymanski <madpole@gmail.com>
2015-03-19 15:23:07 +0100

    in progress: change wrapper so it runs both tests

--- PoCs/ffmpeg_shell/ffmpeg_run_wrapper.sh
+++ PoCs/ffmpeg_shell/ffmpeg_run_wrapper.sh
@@ -2,9 +2,17 @@
 
 # run multiple ffmpegs locally or in docker
 # dirs need to be changed accordingly, or parameterized
+# currently only set for running commands in docker
+# and we are running 2 tests, one after another
+#
+# CMD_LOCAL="/home/ec2-user/cineaste/code/PoCs/ffmpeg_run.sh"
 
-CMD_DOCKER="docker run madpole/pocs /home/crumbles/code/ffmpeg_shell/ffmpeg_docker_run.sh"
-CMD_LOCAL="/home/ec2-user/cineaste/code/PoCs/ffmpeg_run.sh"
+
+CMD_TEST_1="ffmpeg_run_test_01.sh"
+CMD_TEST_2="ffmpeg_run_test_02.sh"
+
+CMD_BASE="docker run madpole/pocs /home/crumbles/code/ffmpeg_shell"
+
 
 
 # ****************************************************************
@@ -18,22 +26,28 @@ function log {
 
 MAX_CLIENTS=5
 
-#$CMD="$CMD_LOCAL"
-CMD="$CMD_DOCKER"
+# ****************************************************************
+# ****  test 1
+# ****************************************************************
+CMD="$CMD_BASE/$CMD_TEST_1"
 
-# sequential
-for (( i=1; i <= $MAX_CLIENTS; i++ ))
+# sequential - just to establish sequential run, we don't have to run 100 times
+for (( i=1; i <= 3; i++ ))
 do
 	CALLER_ID="CALLER_$i"
-	$CMD $CALLER_ID > sequential.$CALLER_ID.log 
+	$CMD $CALLER_ID > $CMD_TEST_1.sequential.$CALLER_ID.log 
 done
 
 # parallel
 for (( i=1; i <= $MAX_CLIENTS; i++ ))
 do
 	CALLER_ID="CALLER_$i"
-	$CMD $CALLER_ID > parallel.$CALLER_ID.log &
+	$CMD $CALLER_ID > $CMD_TEST_1.parallel.$CALLER_ID.log &
 done
+
+
+
+
 
 
 
